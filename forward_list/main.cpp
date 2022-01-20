eb16a70a2806c000a31f5c39550af9d78165c2dc
Ezeobi Uchenna <78647544+jay-uchenna@users.noreply.github.com>
2022-01-21 02:41:39 +0300

    learning forward list

--- forward_list/main.cpp
+++ forward_list/main.cpp
@@ -6,9 +6,48 @@
 //
 
 #include <iostream>
+#include <algorithm>
+#include <forward_list>
+#include <string>
+#include <vector>
+#include <array>
+
+void display(std::forward_list<int>& lst){
+    for(std::forward_list<int>::iterator it = lst.begin(); it != lst.end(); it++ ){
+        std::cout << *it << ", ";
+    }
+    std::cout << "\n";
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::forward_list<int> lst(6, 100);
+    display(lst);
+    
+    lst.push_front(311);
+    display(lst);
+    
+    lst.insert_after(lst.begin(), 311);
+    display(lst);
+    
+    lst.insert_after(lst.before_begin(), 3, 211);
+    display(lst);
+    
+    std::vector<int> vect_test(4, 0);
+    
+    std::forward_list<int>::iterator it = lst.insert_after(++lst.begin(), vect_test.begin(),vect_test.end());
+    display(lst);
+    
+    lst.insert_after(it, 8888);
+    display(lst);
+    
+    lst.reverse();
+    display(lst);
+    
+    lst.erase_after(it, lst.end());
+    display(lst);
+    
+    
+    
+    
     return 0;
 }

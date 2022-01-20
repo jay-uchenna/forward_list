385efb9f5b26001677d48d5676a3c0cd9691a2a0
Ezeobi Uchenna <78647544+jay-uchenna@users.noreply.github.com>
2022-01-21 01:33:25 +0300

    Initial Commit

---
+++ forward_list/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  forward_list
+//
+//  Created by Jedidiah Uchenna on 21.01.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
